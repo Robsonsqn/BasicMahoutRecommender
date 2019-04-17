31d38e4a2718d223ad4401aec2d18a9e79bfa72f
robson_sqn <robson.souza@fncit.com.br>
2019-04-17 13:46:47 -0300

    [RGS] adding csv to resources

--- src/main/java/br/com/fncit/recommender/Recommende.java
+++ src/main/java/br/com/fncit/recommender/Recommende.java
@@ -1,7 +1,6 @@
 package br.com.fncit.recommender;
 
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.model.DataModel;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,12 +8,12 @@ import java.io.IOException;
 public class Recommende {
 
     public FileDataModel getProductsModel() throws IOException {
-       File file = new File("dados.csv");
+       File file = new File("src/main/resources/dados.csv");
        return new FileDataModel(file);
     }
 
     public FileDataModel getCourseModel() throws IOException {
-        File file = new File("cursos_com_questionarios.csv");
+        File file = new File("src/main/resources/cursos_com_questionarios.csv");
         return new FileDataModel(file);
     }
 
